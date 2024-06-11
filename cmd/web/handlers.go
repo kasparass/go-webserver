46073873cfd502521307c84a3f262eaaa8ea8d4c
kasparass <kasparas.urbelis@gmail.com>
2024-06-11 18:17:45 +0300

    unit testing http handlers

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -11,6 +11,10 @@ import (
 	"github.com/kasparass/lets-go/internal/validator"
 )
 
+func ping(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("OK"))
+}
+
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	// Because httprouter matches the "/" path exactly, we can now remove the
 	// manual check of r.URL.Path != "/" from this handler.

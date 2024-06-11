d245b20ae0dd5cf8fe8f72c5a5c8a151d703239a
kasparass <kasparas.urbelis@gmail.com>
2024-06-11 21:07:25 +0300

    end-to-end testing

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -36,6 +36,8 @@ func (app *application) routes() http.Handler {
 	// file will be served (so long as it exists).
 	router.Handler(http.MethodGet, "/static/*filepath", fileServer)
 
+	router.HandlerFunc(http.MethodGet, "/ping", ping)
+
 	// Create a new middleware chain containing the middleware specific to our
 	// dynamic application routes. For now, this chain will only contain the
 	// LoadAndSave session middleware but we'll add more to it later.
